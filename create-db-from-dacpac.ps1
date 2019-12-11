d502d03c5629090bf8f05a50d0060878c896b4f8
Alan Płócieniak <alan-null@outlook.com>
2019-12-11 19:38:17 +0100

    Refactoring
    - added parameters to create-db-from-dacpac instead of constants
    - create-db-from-dacpac is designed to process one file now (not folder)
    - iteration over dacpacs is main responsibility (road to Cmdlet)

--- create-db-from-dacpac.ps1
+++ create-db-from-dacpac.ps1
@@ -5,14 +5,15 @@ param(
     [string]$sqlPassword,
     [parameter(Mandatory = $true, Position = 2)]
     [string]$serverName,
-    [parameter(Mandatory = $false, Position = 3)]
-    [string]$sqlInstallationBinFolder = "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin"
+    [parameter(Mandatory = $true, Position = 3)]
+    $dacPacPath,
+    [parameter(Mandatory = $true, Position = 4)]
+    [string]$dbLocation,
+    [parameter(Mandatory = $false, Position = 5)]
+    [string]$sqlInstallationBinFolder = "C:\Program Files (x86)\Microsoft SQL Server\140\DAC\bin",
+    [parameter(Mandatory = $false, Position = 6)]
+    [string]$tempDbNamePrefix = "PoweShellTransformation."
 )
-
-
-# Const
-$dbLocation = "$PSScriptRoot\temp\dacpac"
-$tempDbNamePrefix = "PoweShellTransformation."
 
 function Test-SQLConnection {
     [OutputType([bool])]
@@ -49,36 +50,37 @@ if (-not (Test-SQLConnection "Data Source=$serverName;database=master;User ID=$s
 }
 
 
-if (-not(Test-Path "$dbLocation\mdf")) {
-    New-Item -ItemType directory -Path "$dbLocation\mdf"
+if (-not(Test-Path "$dbLocation")) {
+    New-Item -ItemType directory -Path "$dbLocation"
 }
 
-$start = Get-Date
-Get-ChildItem -Path $dbLocation | ? { $_.Extension -eq ".dacpac" } | % {
-    $name = $tempDbNamePrefix + $_.Name.Replace(".dacpac", "")
-    $srcPath = $_.FullName
-
-    # publish db from script
-    Write-Host "Processing $($_.Name)" -ForegroundColor Green
-    & "$sqlInstallationBinFolder\SqlPackage.exe" /action:Publish /SourceFile:$srcPath /TargetServerName:$serverName /TargetDatabaseName:$name /TargetUser:$sqlUser /TargetPassword:$sqlPassword
-
-    # get database files paths
-    $query = "select name, physical_name from sys.master_files where name like '$name%' and physical_name like '%$name%'"
-    $paths = Invoke-Sqlcmd -ServerInstance $serverName -Database "master" -Query $query -Username $sqlUser -Password $sqlPassword | % { $_.physical_name }
-
-    $paths | % { Write-Host $_ -ForegroundColor Yellow }
-    # detach database
-    $query = "sp_detach_db '$name', 'true';"
-    Invoke-Sqlcmd -ServerInstance $serverName -Database "master" -Query $query -Username $sqlUser -Password $sqlPassword
-
-    # move mdf and ldf files
-    $paths | % { Move-Item -Path $_ -Destination "$dbLocation\mdf" -Force }
+if (-not(Test-Path $dacPacPath)) {
+    Write-Error "Cannot find file"
 }
+
+$dacPac = Get-Item -Path $dacPacPath
+
+$name = $tempDbNamePrefix + $dacPac.Name.Replace(".dacpac", "")
+$srcPath = $dacPac.FullName
+
+# publish db from script
+Write-Host "Processing $($_.Name)" -ForegroundColor Green
+& "$sqlInstallationBinFolder\SqlPackage.exe" /action:Publish /SourceFile:$srcPath /TargetServerName:$serverName /TargetDatabaseName:$name /TargetUser:$sqlUser /TargetPassword:$sqlPassword
+
+# get database files paths
+$query = "select name, physical_name from sys.master_files where name like '$name%' and physical_name like '%$name%'"
+$paths = Invoke-Sqlcmd -ServerInstance $serverName -Database "master" -Query $query -Username $sqlUser -Password $sqlPassword | % { $_.physical_name }
+
+$paths | % { Write-Host $_ -ForegroundColor Yellow }
+# detach database
+$query = "sp_detach_db '$name', 'true';"
+Invoke-Sqlcmd -ServerInstance $serverName -Database "master" -Query $query -Username $sqlUser -Password $sqlPassword
+
+# move mdf and ldf files
+$paths | % { Move-Item -Path $_ -Destination $dbLocation -Force }
 
 # set file names
-Get-ChildItem -Path "$dbLocation\mdf" | % {
+Get-ChildItem -Path "$dbLocation" | % {
     $newName = $_.Name.Replace("_Primary", "").Replace($tempDbNamePrefix, [string]::Empty)
     Rename-Item -Path $_.FullName -NewName $newName
 }
-$end = Get-Date
-$end - $start
--- main.ps1
+++ main.ps1
@@ -9,76 +9,72 @@ Write-Host "Preparing directory" -ForegroundColor Green
 if (Test-Path -Path .\temp) {
     Write-Host "Removing temporary directories" -ForegroundColor Green
     Remove-Item -Path .\temp -Force -Recurse
+    New-Item -ItemType Directory -Path "temp"
 }
 
-Write-Host "Looking for zip with packages" -ForegroundColor Green
-$archiveFile = Get-ChildItem . -Filter "Sitecore*.zip" | Select-Object -First 1
-if ($archiveFile) {
-    Write-Host "Archive found" -ForegroundColor Green
-    Write-Host "Extracting" -ForegroundColor Green
-    mkdir "temp"
-    Expand-Archive $archiveFile.FullName -DestinationPath "temp"
+Write-Host "Looking for scwdp package" -ForegroundColor Green
+$wdpPackage = Get-ChildItem -Path . -Filter "*single.scwdp.zip" | Select-Object -First 1
+if ($wdpPackage) {
+    Write-Host "scwdp found" -ForegroundColor Green
+    Move-Item -Path $wdpPackage.FullName -Destination "temp"
+    $wdpPackage = Get-Item -Path "temp\$($wdpPackage.Name)"
+    $folderName = $wdpPackage.Name.Replace(" (OnPrem)_single.scwdp", "").Replace(".zip", "")
 
-    Write-Host "Looking for scwdp package" -ForegroundColor Green
-    $wdpPackage = Get-ChildItem -Path ".\temp" -Filter "*single.scwdp.zip" | Select-Object -First 1
-    if ($wdpPackage) {
-        Write-Host "scwdp found" -ForegroundColor Green
-        $folderName = $wdpPackage.Name.Replace(" (OnPrem)_single.scwdp", "").Replace(".zip", "")
+    Write-Host "Extracting 'dacpac' files" -ForegroundColor Green
+    .\7za920\7za.exe e "$($wdpPackage.FullName)" -otemp\dacpac *.dacpac
 
-        Write-Host "Extracting 'dacpac' files" -ForegroundColor Green
-        .\7za920\7za.exe e "$($wdpPackage.FullName)" -otemp\dacpac *.dacpac
+    Write-Host "scwdop archive cleanup" -ForegroundColor Green
+    .\7za920\7za.exe d "$($wdpPackage.FullName)" *.*
 
-        Write-Host "scwdop archive cleanup" -ForegroundColor Green
-        .\7za920\7za.exe d "$($wdpPackage.FullName)" *.*
-
-        Write-Host "Converting databases" -ForegroundColor Green
-        .\create-db-from-dacpac.ps1 $config.sqlUser $config.sqlPassword $config.serverName
-
-
-        mkdir "temp\Data\packages"
-        Write-Host "Packaging databases" -ForegroundColor Green
-
-        Set-Location .\7za920
-        $path = "$($wdpPackage.FullName)".Replace($PSScriptRoot, "..")
-
-        Write-Host "Adding databases to archive" -ForegroundColor Green
-        7za a $path ..\temp\dacpac\mdf
-        7za rn $path "mdf" "$folderName\Databases"
-
-        Write-Host "Adding Data folder to archive" -ForegroundColor Green
-        7za a $path ..\temp\Data
-        7za rn $path "Data" "$folderName\Data"
-
-        7za rn $path "Content" "$folderName"
-        Set-Location ..
-
-        if (Test-Path "$PSScriptRoot\$($wdpPackage.Name)") {
-            Write-Host "File '$PSScriptRoot\$($wdpPackage.Name)' already exists" -ForegroundColor Yellow
-            $confirmation = Read-Host "Do you want to overwrite"
-            if ($confirmation -eq 'y') {
-                Remove-Item -Path $PSScriptRoot\$($wdpPackage.Name)
-            }
-            else {
-                Write-Host "Script aborted"
-                return
-            }
-        }
-
-        Write-Host "Moving processed wdp to outp folder" -ForegroundColor Green
-        Move-Item -Path $wdpPackage.FullName -Destination $PSScriptRoot
-        $wdp = Get-ChildItem -Path "." -Filter "*single.scwdp.zip" | Select-Object -First 1
-
-        Write-Host "Renaming package" -ForegroundColor Green
-        Write-Host "$PSScriptRoot\$folderName.1click" -ForegroundColor Green
-        $fileName = "$folderName.1click"
-        if (Test-Path "$PSScriptRoot\$folderName.1click") {
-            Write-Host "File with that name already exists" -ForegroundColor Green
-            $postfix = (New-Guid).ToString()
-            $fileName = "$folderName-$postfix.1click"
-            Write-Host "New file name: '$fileName'" -ForegroundColor Green
-        }
-        Rename-Item -Path $wdp.FullName -NewName $fileName
+    Write-Host "Converting databases" -ForegroundColor Green
+    $dbLocation = ".\temp\dacpac"
+    Get-ChildItem -Path $dbLocation | ? { $_.Extension -eq ".dacpac" } | % {
+        .\create-db-from-dacpac.ps1 $config.sqlUser $config.sqlPassword $config.serverName $_.FullName "$dbLocation\mdf"
     }
+
+    mkdir "temp\Data\packages"
+    Write-Host "Packaging databases" -ForegroundColor Green
+
+    Set-Location .\7za920
+    $path = "$($wdpPackage.FullName)".Replace($PSScriptRoot, "..")
+
+    Write-Host "Adding databases to archive" -ForegroundColor Green
+    7za a $path ..\temp\dacpac\mdf
+    7za rn $path "mdf" "$folderName\Databases"
+
+    Write-Host "Adding Data folder to archive" -ForegroundColor Green
+    7za a $path ..\temp\Data
+    7za rn $path "Data" "$folderName\Data"
+
+    7za rn $path "Content" "$folderName"
+    Set-Location ..
+
+    if (Test-Path "$PSScriptRoot\$($wdpPackage.Name)") {
+        Write-Host "File '$PSScriptRoot\$($wdpPackage.Name)' already exists" -ForegroundColor Yellow
+        $confirmation = Read-Host "Do you want to overwrite"
+        if ($confirmation -eq 'y') {
+            Remove-Item -Path $PSScriptRoot\$($wdpPackage.Name)
+        }
+        else {
+            Write-Host "Script aborted"
+            return
+        }
+    }
+
+    Write-Host "Moving processed wdp to outp folder" -ForegroundColor Green
+    Move-Item -Path $wdpPackage.FullName -Destination $PSScriptRoot
+    $wdp = Get-ChildItem -Path "." -Filter "*single.scwdp.zip" | Select-Object -First 1
+
+    Write-Host "Renaming package" -ForegroundColor Green
+    Write-Host "$PSScriptRoot\$folderName.1click" -ForegroundColor Green
+    $fileName = "$folderName.1click"
+    if (Test-Path "$PSScriptRoot\$folderName.1click") {
+        Write-Host "File with that name already exists" -ForegroundColor Green
+        $postfix = (New-Guid).ToString()
+        $fileName = "$folderName-$postfix.1click"
+        Write-Host "New file name: '$fileName'" -ForegroundColor Green
+    }
+    Rename-Item -Path $wdp.FullName -NewName $fileName
 }
 
 Write-Host "Removing temp folder" -ForegroundColor Green
